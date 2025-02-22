931bced2fac6b285b8d1a1b484e06948a9c3d715
Code-By-Tom2 <shovanchaudhauri2019@gmail.com>
2025-02-22 23:14:28 +0530

    js project

---
+++ atomatic-pasword.js/script.js
@@ -0,0 +1,11 @@
+function generatePassword(length) {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+    return password;
+}
+
+console.log(generatePassword(12)); // Generates a 12-character random password
